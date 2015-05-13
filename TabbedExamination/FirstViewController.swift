13afe7f6af32f4e65d1bf80962b54a90bfc50e96
Lee-kai Wang <free.leekai@gmail.com>
2015-05-13 12:06:00 +0800

    Initial Commit

---
+++ TabbedExamination/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedExamination
+//
+//  Created by Lee-kai Wang on 5/13/15.
+//  Copyright (c) 2015 Lee-kai Wang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TabbedExamination/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabbedExamination
+//
+//  Created by Lee-kai Wang on 5/13/15.
+//  Copyright (c) 2015 Lee-kai Wang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
