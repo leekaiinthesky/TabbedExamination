d62832259ab1159835ac66d75f246d4a43012851
Lee-kai Wang <free.leekai@gmail.com>
2015-05-14 16:19:08 +0800

    add camera preview

--- TabbedExamination/FirstViewController.swift
+++ TabbedExamination/FirstViewController.swift
@@ -7,35 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class FirstViewController: UIViewController {
     
     var creationTimes = [NSDate]()
     var completionTimes = [NSDate]()
     var instructionLabel = UILabel()
-    var scoreLabel = UILabel()
+    //var scoreLabel = UILabel()
     var activeBubble = UIView()
 
+    let captureSession = AVCaptureSession()
+    var captureDevice: AVCaptureDevice!
+    var output = AVCaptureMovieFileOutput()
+    var topPreviewLayer : AVCaptureVideoPreviewLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let devices = AVCaptureDevice.devices()
+        captureSession.sessionPreset = AVCaptureSessionPresetMedium // can be changed
+        // Loop through all the capture devices on this phone
+        for device in devices {
+            if device.hasMediaType(AVMediaTypeVideo) {
+                if device.position == AVCaptureDevicePosition.Front {
+                    captureDevice = device as? AVCaptureDevice
+                    if captureDevice != nil {
+                        beginSession()
+                    }
+                }
+            }
+        }
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        scoreLabel.removeFromSuperview()
+        //scoreLabel.removeFromSuperview()
         instructionLabel.removeFromSuperview()
         activeBubble.removeFromSuperview()
         creationTimes = []
         completionTimes = []
 
-        instructionLabel.frame = CGRect(x: 150, y: 100, width: view.frame.width - 2 * 150, height: 500)
+        instructionLabel.frame = CGRect(x: 50, y: 100, width: view.frame.width - 2 * 50, height: 500)
         instructionLabel.text = "Tap 10 red squares as fast as you can. Tap the green square below to begin."
         instructionLabel.font = UIFont(name: instructionLabel.font.fontName, size: 36)
         instructionLabel.numberOfLines = 0
         instructionLabel.textAlignment = NSTextAlignment.Center
+        instructionLabel.backgroundColor = UIColor.whiteColor()
         view.addSubview(instructionLabel)
         
         let startingRadius = 40
@@ -63,7 +83,7 @@ class FirstViewController: UIViewController {
     }
     
     func destroyBubbleAndCreate() {
-        scoreLabel.removeFromSuperview()
+        //scoreLabel.removeFromSuperview()
         instructionLabel.removeFromSuperview()
         activeBubble.removeFromSuperview()
         
@@ -83,18 +103,19 @@ class FirstViewController: UIViewController {
                 totalTime += time
             }
             
-            scoreLabel.frame = CGRect(x: 200, y: 100, width: view.frame.width - 2 * 200, height: 400)
-            scoreLabel.text = "Fastest: \(Int(1000 * bestTime)) ms\nSlowest: \(Int(1000 * worstTime)) ms\nAverage: \(Int(1000 * totalTime / 10)) ms"
-            scoreLabel.font = UIFont(name: scoreLabel.font.fontName, size: 36)
-            scoreLabel.numberOfLines = 0
-            scoreLabel.textAlignment = NSTextAlignment.Center
-            view.addSubview(scoreLabel)
+//            scoreLabel.frame = CGRect(x: 200, y: 100, width: view.frame.width - 2 * 200, height: 400)
+//            scoreLabel.text = "Fastest: \(Int(1000 * bestTime)) ms\nSlowest: \(Int(1000 * worstTime)) ms\nAverage: \(Int(1000 * totalTime / 10)) ms"
+//            scoreLabel.font = UIFont(name: scoreLabel.font.fontName, size: 36)
+//            scoreLabel.numberOfLines = 0
+//            scoreLabel.textAlignment = NSTextAlignment.Center
+//            scoreLabel.backgroundColor = UIColor.grayColor()
+//            view.addSubview(scoreLabel)
             
             creationTimes = []
             completionTimes = []
             
-            instructionLabel.frame = CGRect(x: 150, y: 350, width: view.frame.width - 2 * 150, height: 200)
-            instructionLabel.text = "Tap the green square to restart."
+            instructionLabel.frame = CGRect(x: 150, y: 100, width: view.frame.width - 2 * 150, height: 500)
+            instructionLabel.text = "Fastest: \(Int(1000 * bestTime)) ms\nSlowest: \(Int(1000 * worstTime)) ms\nAverage: \(Int(1000 * totalTime / 10)) ms\n\n\nTap the green square to restart."
             instructionLabel.font = UIFont(name: instructionLabel.font.fontName, size: 36)
             instructionLabel.numberOfLines = 0
             instructionLabel.textAlignment = NSTextAlignment.Center
@@ -125,4 +146,42 @@ class FirstViewController: UIViewController {
         }
     }
     
+    func beginSession() {
+        
+        if let device = captureDevice {
+            device.lockForConfiguration(nil)
+            device.whiteBalanceMode = AVCaptureWhiteBalanceMode.Locked
+            device.unlockForConfiguration()
+        }
+        
+        var err: NSError? = nil
+        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
+        //captureSession.addOutput(videoDataOutput)
+        
+        if err != nil {
+            println("error: \(err?.localizedDescription)")
+        }
+        
+        topPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        topPreviewLayer.videoGravity = AVLayerVideoGravityResize
+        view.layer.addSublayer(topPreviewLayer)
+        topPreviewLayer.zPosition = -2000
+        topPreviewLayer.frame = view.bounds
+        
+        //        bottomPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        //        bottomPreviewLayer!.videoGravity = AVLayerVideoGravityResize
+        //        bottomView.layer.addSublayer(bottomPreviewLayer)
+        //        bottomPreviewLayer?.frame = bottomView.bounds
+        //        println(bottomPreviewLayer?.transform)
+        
+//        output = AVCaptureVideoDataOutput()
+//        output?.alwaysDiscardsLateVideoFrames = true // I think this is default anyway
+//        output?.videoSettings = [kCVPixelBufferPixelFormatTypeKey: kCVPixelFormatType_32BGRA]
+//        let cameraQueue = dispatch_queue_create("cameraQueue", DISPATCH_QUEUE_SERIAL)
+//        output?.setSampleBufferDelegate(self, queue: cameraQueue)
+//        captureSession.addOutput(output)
+        
+        
+        captureSession.startRunning()
+    }
 }
